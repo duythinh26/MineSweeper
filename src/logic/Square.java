8127e61d96cb80c96276b1d9a76d17bdd51ddace
duythinh26 <pduythinh79@gmail.com>
2021-05-20 15:40:25 +0700

    feat(src/logic): add file Square.java

---
+++ src/logic/Square.java
@@ -0,0 +1,46 @@
+package logic;
+
+public class Square {
+    private boolean isOpen;
+    private boolean hasMine;
+    private boolean isTarget;
+    private int numMineAround;
+
+    public Square() {
+        isOpen = false;
+        hasMine = false;
+        isTarget = false;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setIsOpen(boolean isOpen){
+        this.isOpen = isOpen;
+    }
+
+    public boolean hasMine() {
+        return hasMine;
+    }
+
+    public void setMine(boolean hasMine) {
+        this.hasMine = hasMine;
+    }
+
+    public boolean isTarget() {
+        return isTarget;
+    }
+
+    public void setTarget(boolean isTarget) {
+        this.isTarget = isTarget;
+    }
+
+    public int getNumMineAround() {
+        return numMineAround;
+    }
+
+    public void setNumMineAround(int numMineAround) {
+        this.numMineAround = numMineAround;
+    }
+}
