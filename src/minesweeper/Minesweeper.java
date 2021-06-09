e3d6882c7393fa8e0e72b532f23e47dce8f33ef0
duythinh26 <pduythinh79@gmail.com>
2021-06-09 15:46:26 +0700

    update Minesweeper.java

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -1,25 +1,44 @@
 package minesweeper;
 import java.awt.BorderLayout;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
+import javax.swing.JPanel;
 import logic.Board;
 
 public class Minesweeper extends JFrame {
     private JLabel status;
+    private JPanel buttonPanel;
+    private JButton undoButton;
+    private JButton rulesButton;
+    protected Board board;
 
-    public Minesweeper() {
+    public Minesweeper() throws IOException {
+        buttonPanel = new JPanel();
+        undoButton = new JButton("Undo");
+        rulesButton = new JButton("Rules");
     	status = new JLabel("");
+
+        add(buttonPanel, BorderLayout.NORTH);
+        buttonPanel.add(undoButton, BorderLayout.SOUTH);
+        buttonPanel.add(rulesButton, BorderLayout.NORTH);
+
         add(status, BorderLayout.SOUTH);
-        add(new Board(status));
+        
+        board = new Board(status, undoButton, rulesButton);
+        add(board);
+
         setResizable(false);
         pack();
+
         setTitle("Minesweeper");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
      	Minesweeper ms = new Minesweeper();
     	ms.setVisible(true);
     }
