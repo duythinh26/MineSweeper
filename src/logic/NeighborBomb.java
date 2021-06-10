66ed13ff677f2ac43ebdb3be1152fe48c5bb0efa
duythinh26 <pduythinh79@gmail.com>
2021-06-10 18:06:28 +0700

    feat(logic): add file NeighborBomb.java

---
+++ src/logic/NeighborBomb.java
@@ -0,0 +1,31 @@
+package logic;
+
+import gui.SquareType;
+
+public class NeighborBomb extends Square{
+
+    private int bombSquareCount;
+	
+	public NeighborBomb() {
+		super();
+        this.squareType = SquareType.BombNeighbor;
+	}
+	
+	
+	public NeighborBomb(String isCovered, String isMarked, int bombCellCount) {
+		super(isCovered, isMarked);
+		this.squareType = SquareType.BombNeighbor;
+		this.bombSquareCount = bombSquareCount;
+	}
+	
+	//want to call this function when initializing board, which automatically  
+	@Override
+        public void squareCount() {
+		this.bombSquareCount ++;
+	}
+        
+    @Override
+    public SquareType getSquareType() {
+        return this.squareType;
+    }
+}
